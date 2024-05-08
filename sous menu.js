d0a8d0a93c9145df01bc9307fc2bd8eb1cbcfc09
Arielogrin <gringomoi515@gmail.com>
2024-05-08 13:06:57 +0100

    Add files via upload

---
+++ sous menu.js
@@ -0,0 +1,8 @@
+javascript
+document.getElementById("A propos").addEventListener("mouseover",function(){
+    document.getElementById("aide").style.display="block";
+});
+
+document.getElementById("A propos").addEventListener("mouseout",function(){
+    document.getElementById("aide").style.display="none";
+});
